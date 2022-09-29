f57ea1190130d7ee232f611359ab47cfe0740a3b
huseyinavnisen <havnisen@gmail.com>
2022-09-29 22:13:11 +0300

    30.09.2022

---
+++ src/test/java/pages/FacebookPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class FacebookPage {
+
+   public FacebookPage(){
+       PageFactory.initElements(Driver.getDriver(),this);
+   }
+    //email
+    @FindBy(xpath = "//*[@name='email']")
+    public WebElement email;
+
+    //sifre
+    @FindBy(xpath = "//*[@id='pass']")
+    public WebElement sifre;
+
+    //Giris
+    @FindBy(xpath = "//*[@name='login']")
+    public WebElement giris;
+
+    @FindBy(xpath = "//*[@class='_9ay7']")
+    public WebElement hataliGiris;
+
+    // ccokies
+    @FindBy(xpath = "//button[text()='Allow essential and optional cookies']")
+    public WebElement cookies;
+}
---
+++ src/test/java/tests/Day18_POM/C01_Facebook_Test.java
@@ -0,0 +1,33 @@
+package tests.Day18_POM;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.Driver;
+
+public class C01_Facebook_Test {
+    @Test
+    public void test1() {
+        // https://www.facebook.com/ adresine gidin
+        Driver.getDriver().get("https://www.facebook.com/");
+        // POM’a uygun olarak email, sifre kutularini ve giris yap butonunu locate edin
+        FacebookPage facebookPage=new FacebookPage();
+        // Faker class’ini kullanarak email ve sifre degerlerini
+        Faker faker=new Faker();
+        String mail=faker.internet().emailAddress();
+        String sifre=faker.internet().password();
+        facebookPage.email.sendKeys(mail);
+        facebookPage.sifre.sendKeys(sifre);
+        System.out.println("mail = " + mail);
+        System.out.println("sifre = " + sifre);
+
+        // girip, giris butonuna basin
+        facebookPage.giris.click();
+        // Basarili giris yapilamadigini test edin
+        Assert.assertTrue(facebookPage.hataliGiris.isDisplayed());
+
+    }
+
+
+}
