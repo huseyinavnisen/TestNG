d63b779b5613f596555ee21fd2e6fc02cb2181d6
huseyinavnisen <havnisen@gmail.com>
2022-09-29 22:42:39 +0300

    30.09.2022

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,4 +1,27 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+    public static Properties properties;
+    static { // her methoddan önce çalışır
+      String dosyaYolu="target/configuration.properties";
+
+      try {
+            FileInputStream fis=new FileInputStream(dosyaYolu);
+            properties= new Properties();
+            properties.load(fis); // fis in okuduğu bilgileri prpoerties'e yükledi
+
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+    }
+
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
 }
